aee019b7bd65067db46f74c6ce7a00fa200163a7
bumba <pkmnbumba@gmail.com>
2017-02-11 19:24:46 -0800

    added in comments for unobtainable shinies

--- src/checks/shiny.js
+++ src/checks/shiny.js
@@ -2,9 +2,33 @@ module.exports = {
   description: 'Certain Pokemon cannot be obtained shiny.',
   field: 'isShiny',
   filter (pkmn) {
-    return [251, 494, 647, 648, 718, 720, 721, 785, 786,
-      787, 788, 789, 790, 791, 792, 793, 794, 795, 796, 797,
-      798, 799, 800, 801, 802].includes(pkmn.dexNo);
+    return [
+      251, // Celebi
+      494, // Victini
+      647, // Keldeo
+      648, // Meloetta
+      718, // Zygarde
+      720, // Hoopa
+      721, // Volcanion
+      785, // Tapu Koko
+      786, // Tapu Lele
+      787, // Tapu Bulu
+      788, // Tapu Fini
+      789, // Cosmog
+      790, // Cosmoem
+      791, // Solgaleo
+      792, // Lunala
+      793, // Nihilego
+      794, // Buzzwole
+      795, // Pheromosa
+      796, // Xurkitree
+      797, // Celesteela
+      798, // Kartana
+      799, // Guzlord
+      800, // Necrozma
+      801, // Magearna
+      802 // Marshadow
+    ].includes(pkmn.dexNo);
   },
   test (pkmn) {
     return !pkmn.isShiny;
