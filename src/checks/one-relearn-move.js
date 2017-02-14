e8d42b94e0b6224eb1ddf6753071d5c553a7bb23
pkmnbumba <pkmnbumba@gmail.com>
2017-02-13 22:13:00 -0800

    updated description and filter

--- src/checks/one-relearn-move.js
+++ src/checks/one-relearn-move.js
@@ -1,10 +1,10 @@
 const gameCheck = require('../helpers/gameId.js');
 
 module.exports = {
-  description: 'Pokemon hatched from an egg in gen 6-7 must have at least one relearn move.',
-  field: 'eggMoveName',
+  description: 'Pokemon hatched in gen 6-7 must have a relearn move while Pokemon transferred from older gens should not.',
+  field: 'eggMove1Id',
   filter (pkmn) {
-    return !(pkmn.eggLocationId === 0);
+    return !(pkmn.eggLocationId === 0) || gameCheck.originGen(pkmn.otGameId) < 6;
   },
   test (pkmn) {
     if (gameCheck.originGen(pkmn.otGameId) < 6) {
