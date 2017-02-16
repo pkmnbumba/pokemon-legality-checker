820efa2eaf4e30ba3bbb85f273a97d6e106aac41
pkmnbumba <pkmnbumba@gmail.com>
2017-02-16 11:36:23 -0800

    last fix

--- src/checks/one-relearn-move.js
+++ src/checks/one-relearn-move.js
@@ -4,7 +4,7 @@ module.exports = {
   description: 'Pokemon hatched in gen 6-7 must have a relearn move while Pokemon transferred from older gens should not.',
   field: 'eggMove1Id',
   filter (pkmn) {
-    return !(pkmn.eggLocationId === 0) || gameCheck.originGen(pkmn.otGameId) < 6;
+    return pkmn.eggLocationId !== 0 || gameCheck.originGen(pkmn.otGameId) < 6;
   },
   test (pkmn) {
     if (gameCheck.originGen(pkmn.otGameId) < 6) {
